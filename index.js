dce3d5c1c2e5302cffa5e0d13a9b0da5e07868ac
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-08-24 20:00:12 -0500

    fixed display output

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-onEvent("nextBtn","click",function(){
-  var pNoun = getText("pNounOutput");
+onEvent("nextBtn", "click", function() {
+  var pNoun = getText("pNounOutput"); 
   var noun1 = getText("nOutput1");
   var noun2 = getText("nOutput2");
   var song = getText("songOutput");
   var verb = getText("verbOutput");
-  console.log(showUserInput);
+  console.log("nextbuttonclicked");
   var userMessage = "Learning to drive is a tricky process. There are a few rules you must follow."+ "\n"+ "\n" + "1. Keep two "+ pNoun+ " on the steering wheel at all times."+ "\n" +"\n" + "2. Step on the " + noun1+ " to speed up and the " + noun2 + " to slow down"+ "\n"+ "\n" +"3. Your parents will just LOVE it if you play" + song+ " on the radio." +"\n" + "\n"+"4. Make sure to honk your horn when you see " + verb +" on a street sign.";
-  console.log(userMessage);
-  setScreen("screen2.html");
-  setText("textOutput", userMessage);
+  setText("displayOutput", userMessage);
+  document.getElementById("form-input").classList.add("d-none");
+  document.getElementById("resultContainer").classList.remove("d-none");
+});
+
+onEvent("startOver", "click", function() {
+  setScreen("index.html");
+  setText("pNounOutput","");
+  setText("nOutput1","");
+  setText("nOutput2","");
+  setText("songOutput","");
+  setText("verbOutput","");
+  document.getElementById("form-input").classList.remove("d-none");
+  document.getElementById("resultContainer").classList.add("d-none");
 });
--- screen2.js
+++
@@ -1,8 +0,0 @@
-onEvent("startOver", "click",function(){     
-  setScreen("collectUserInput");
-  setText("pNounOutput","");
-  setText("nOutput1","");
-  setText("nOutput2","");
-  setText("songOutput","");
-  setText("verbOutput","");
-});
